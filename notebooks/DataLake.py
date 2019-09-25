5a2e7ab7c23674fb658630d96cc142a83449a25d
Philip Domingo <philipdomingo@outlook.com>
2019-09-25 10:39:17 +0800

    Added markdown docs

--- notebooks/DataLake.py
+++ notebooks/DataLake.py
@@ -1,1 +1,9 @@
 # Databricks notebook source
+# MAGIC %md 
+# MAGIC 
+# MAGIC #### How to read (and write) data from Datalake using Databricks
+# MAGIC 
+# MAGIC Reference Links:
+# MAGIC   1. Azure Data Lake Gen1: https://docs.databricks.com/spark/latest/data-sources/azure/azure-datalake.html
+# MAGIC   2. Azure Data Lake Gen2: https://docs.databricks.com/spark/latest/data-sources/azure/azure-datalake-gen2.html
+# MAGIC   3. Create Azure AD Application: https://docs.microsoft.com/en-us/azure/active-directory/develop/howto-create-service-principal-portal
